c534ecd5dd9586d58b06e608c6ff9bcef6942b64
Lambo-555 <lambo-555@yandex.ru>
2020-12-22 01:03:17 +0300

    add long string cutter

--- utils/messages.js
+++ utils/messages.js
@@ -4,8 +4,8 @@ const config = require('../config/config');
 //______________ MESSAGE CREATOR ___________________
 function msg(name, message, socketId, isBot = true) {
   return {
-    name: stringProtection(name),
-    message: stringProtection(message),
+    name: stringProtection(name, 20),
+    message: stringProtection(message, 250),
     time: moment().format(config.DATE_FORMAT),
     socketId: socketId,
     isBot: !!isBot
@@ -13,8 +13,10 @@ function msg(name, message, socketId, isBot = true) {
 }
 
 // replace dangerous chars in strings
-function stringProtection(string) {
-  return string.replace(/[&<>"']/g, '');
+// slice long strings
+function stringProtection(string, len) {
+  const sliced = string.slice(0, len);
+  return sliced.replace(/[&<>"']/g, '');
 }
 
 module.exports = msg;
